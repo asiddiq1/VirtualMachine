90c579f563187ad4db1d57cfeb74629bf5aa4415
aisha <12617103+asiddiq1@users.noreply.github.com>
2023-06-23 17:11:20 -0700

    initial lc3 memory, registers, instruction set

---
+++ lc3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+/* unix only */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX]; //65536 memory locations (128KB)
+
+//8 gen purpose registers, allow to store data in CPU
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, //program counter
+    R_COND, //cond flag
+    R_COUNT
+};
+
+uint16_t reg[R_COUNT]; //store registers in array
+
+
+//instruction set that tells the CPU to do some task
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
+
+//conditional flags to check logical conditions
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
